1c44d1c14d270511d33ac71f69afbd8edfe81b9e
halodong <1661089448@qq.com>
2021-02-26 20:39:34 +0800

    local保存用户信息

---
+++ src/utils/storage.js
@@ -0,0 +1,34 @@
+/*
+进行local数据存储管理的工具模块
+ */
+import store from 'store'
+const USER_KEY = 'user_key'
+const USER_ID = '_id'
+let storage = {
+  /*
+  保存user
+   */
+  saveUser (user , id) {
+    // localStorage.setItem(USER_KEY, JSON.stringify(user))
+    store.set(USER_KEY, user)
+    store.set(USER_ID, id)
+  },
+
+  /*
+  读取user
+   */
+  getUser () {
+    // return JSON.parse(localStorage.getItem(USER_KEY) || '{}')
+    return store.get(USER_KEY , USER_ID) || {}
+  },
+
+  /*
+  删除user
+   */
+  removeUser () {
+    // localStorage.removeItem(USER_KEY)
+    store.remove(USER_KEY , USER_ID)
+  }
+}
+
+export default storage

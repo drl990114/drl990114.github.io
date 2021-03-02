0cd1c946e97d305aa10c4f182327ea8618f09311
halodong <1661089448@qq.com>
2021-03-02 15:47:55 +0800

    解析url参数方法

---
+++ src/utils/getParam.js
@@ -0,0 +1,9 @@
+
+export function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+ }

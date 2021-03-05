95678623d606671bb2d409a9ed216a3fe312f632
halodong <1661089448@qq.com>
2021-03-05 19:49:32 +0800

    首页头部

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,40 +11,49 @@ const { Search } = Input;
 export function Header() {
     const history = useHistory()
     const nowPath = history.location.pathname
-    console.log(nowPath)
     const [selectKey, setKey] = useState(nowPath)
     const handleSelect = (key) => {
         setKey(key)
         history.push(key)
     }
+
+    const handleSearch = (value) => {
+        history.replace(`/scan/search?desc=${value}`)
+        window.location.reload()
+    }
     return (
-            <Row
-                align='middle'>
-                <Col xs={4} sm={4} md={3} lg={4} xl={5} > 
-                <img style={{ width: '40px' }} src={shi} alt='logo'></img>
-                <img style={{ width: '40px' }} src={zhe} alt='logo'></img>
-                </Col>
-                <Col xs={7} sm={7} md={8} lg={7} xl={6}>
-                    <Menu
-                        className='menu'
-                        mode="horizontal"
-                        selectedKeys={selectKey}
-                        onSelect={(e) => handleSelect(e.key)}>
-                        <Menu.Item key="/scan/home" >
-                            首页
+        <Row
+            align='middle'>
+            <Col xs={4} sm={4} md={3} lg={4} xl={5} >
+                <a href='/scan/home'>
+                    <img style={{ width: '40px' }} src={shi} alt='logo'></img>
+                    <img style={{ width: '40px' }} src={zhe} alt='logo'></img>
+                </a>
+
+            </Col>
+            <Col xs={7} sm={7} md={8} lg={7} xl={6}>
+                <Menu
+                    className='menu'
+                    mode="horizontal"
+                    selectedKeys={selectKey}
+                    onSelect={(e) => handleSelect(e.key)}>
+                    <Menu.Item key="/scan/home" >
+                        首页
                  </Menu.Item>
-                        <Menu.Item key="/scan/contact" >
-                            投稿
+                    <Menu.Item key="/scan/contact" >
+                        投稿
                  </Menu.Item>
-                    </Menu>
-                </Col>
-                <Col xs={9} sm={9} md={8} lg={7} xl={7}>
-                    <Space>
-                        <Search placeholder="请输入你想搜索的内容" allowClear onSearch={''} />
-                    </Space></Col>
-                <Col xs={4} sm={4} md={5} lg={6} xl={6}>
-                   {memory.user === '' ?  <Button className='btn' size='large' onClick={()=>history.push('/login')}>登陆/注册</Button> : <span>Hello, {memory.user}</span>}</Col>
-            </Row>
+                </Menu>
+            </Col>
+            <Col xs={9} sm={9} md={8} lg={7} xl={7}>
+                <Space>
+                    <Search placeholder="请输入你想搜索的内容" allowClear onSearch={(value)=>handleSearch(value)} />
+                </Space></Col>
+            <Col xs={4} sm={4} md={5} lg={6} xl={6}>
+                {memory.user === '' ? 
+                <Button className='btn' size='large' onClick={() => history.push('/login')}>登陆/注册</Button> : 
+                <a href={`/personal?id=${memory.id}`}>Hello, {memory.user}</a>}</Col>
+        </Row>
 
 
     )

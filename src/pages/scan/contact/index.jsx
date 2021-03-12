04aec2b8cb26d785321e780d3de83a1e0fec9879
halodong <1661089448@qq.com>
2021-03-12 18:31:33 +0800

    跳转优化

--- src/pages/scan/contact/index.jsx
+++ src/pages/scan/contact/index.jsx
@@ -8,8 +8,7 @@ export const Contact = (props) => {
         <span>可以附带你的诗，发送至15511050325@163.com，邮件标题请注明“诗者投稿”</span>
         <br/>
         <br/>
-        <span>对此开源作品有更好的想法，可以在<a href='https://github.com/halodong/shizhe'>github</a>上给我留言</span>
-        
+        <span>对此开源作品有更好的想法，可以在<a target='blank' href='https://github.com/halodong/shizhe'>github</a>上给我留言</span>
         </div>
 
     )

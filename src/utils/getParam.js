e634ee8aafd078ec259d37c896ecd2ab5915c608
halodong <1661089448@qq.com>
2021-03-12 10:25:06 +0800

    更新解析url函数

--- src/utils/getParam.js
+++ src/utils/getParam.js
@@ -2,7 +2,7 @@
 //解析当前url中参数的方法，name为参数名
 export function getQueryString(name) {
     let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    let r = window.location.search.substr(1).match(reg);
+    let r = window.location.hash.split('?')[1].match(reg);
     if (r != null) {
         return decodeURIComponent(r[2]);
     };

54fcfe9295edc6b7e9077b8c361b531402157998
halodong <1661089448@qq.com>
2021-05-30 20:32:05 +0800

    页面结构调整

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <Body />
+    <Foot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "../components/home/Header.vue";
+import Body from "../components/home/Body.vue";
+import Foot from "../components/home/Foot.vue";
+export default defineComponent({
+  name: "Home",
+  components: {
+    Header,
+    Body,
+    Foot,
+  },
+});
+</script>
+
+<style scoped>
+.home {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 100%;
+  width: 100%;
+  text-align: left;
+}
+</style>

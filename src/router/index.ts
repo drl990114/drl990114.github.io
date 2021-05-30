99cbf06bccf10ba400acba34a46e0b31929e4e95
halodong <1661089448@qq.com>
2021-05-30 20:32:58 +0800

    优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../page/Home.vue'
-import Read from '../page/Read.vue'
-import Welcome from '../page/Welcome.vue'
-import About from '../page/About.vue'
-import Login from '../page/admin/Login.vue'
+import Home from '../views/Home.vue'
+import Read from '../views/Read.vue'
+import Welcome from '../views/Welcome.vue'
+import About from '../views/About.vue'
+import Login from '../views/admin/Login.vue'
+import ShowData from '../views/ShowData.vue'
+import Write from "../views/Write.vue"
 import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
@@ -31,9 +33,19 @@ const routes = [
     component: About
   },
   {
+    path: "/showdata",
+    name: "showdata",
+    component: ShowData
+  },
+  {
     path: "/login",
     name: "login",
     component: Login
+  },
+   {
+    path: "/write",
+    name: "write",
+    component: Write
   }
 ]
 
---
+++ src/views/Welcome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="welcome">
+    <h1>Welcome</h1>
+    <div class="nav">
+      <router-link
+        class="link"
+        v-for="item in navList"
+        :key="item"
+        :to="item.link"
+        >{{ item.name }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { navigationList } from "../utils/list";
+
+export default defineComponent({
+  name: "Welcome",
+  data() {
+    return { navList: navigationList };
+  },
+});
+</script>
+
+<style scoped>
+.welcome {
+  width: 100%;
+  margin: 0 auto;
+}
+.nav {
+  width: 40%;
+  margin: 20px auto;
+  padding: 40px 30px;
+  background: #ecf0f3;
+  border-radius: 20px;
+  font-size: 1.5rem;
+  box-shadow: -6px -6px 20px rgba(255, 255, 255, 1),
+    6px 6px 20px rgba(0, 0, 0, 0.1);
+}
+.link {
+  margin: 4px;
+}
+</style>

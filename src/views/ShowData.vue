96317545d72e02a34d001852f84c5592fcb56b7b
halodong <1661089448@qq.com>
2021-05-30 20:32:19 +0800

    展示数据路由

---
+++ src/views/ShowData.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="showdata">
+  <Header/>
+  <Body/>
+</div>
+
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import Header from '../components/showdata/header.vue'
+import Body from '../components/showdata/body.vue'
+export default defineComponent({
+  name: 'ShowData',
+  components:{
+      Header,
+      Body
+  }
+})
+</script>
+
+<style scoped>
+.showdata{
+    width:100% ;
+    margin: auto;
+    word-break: break-all;
+    word-wrap: break-word;
+}
+</style>

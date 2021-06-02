c942f235ce37aa227bb91fcf2cf6256e4614c292
halodong <1661089448@qq.com>
2021-06-02 21:23:15 +0800

    增加：server.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const vuePlugin = require('@vitejs/plugin-vue')
+
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+module.exports = {
+  plugins: [
+    vuePlugin(),
+    {
+      name: 'virtual',
+      resolveId(id) {
+        if (id === '@foo') {
+          return id
+        }
+      },
+      load(id) {
+        if (id === '@foo') {
+          return `export default { msg: 'hi' }`
+        }
+      }
+    }
+  ],
+  build: {
+    minify: false
+  }
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  optimizeDeps: {
-    include: ['@kangc/v-md-editor/lib/theme/vuepress.js'],
-  },
-})
+// module.exports={
+//   plugins: [    vuePlugin(),],
+//   optimizeDeps: {
+//     include: ['@kangc/v-md-editor/lib/theme/vuepress.js'],
+//   },
+//   build: {
+//     minify: false
+//   }
+// }

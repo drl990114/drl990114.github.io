0a2352df9f69790cae317c64112f6c5dee739ee6
halodong <1661089448@qq.com>
2021-06-19 22:14:33 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,12 @@
 const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   theme: require.resolve('../../theme-deeply'), // 使用本地主题
-
+  head: [
+    [
+      'link',
+      { rel: 'icon', href: 'favicon.ico' }
+    ]
+  ],
   title: "RuiLin Dong's blog",
   description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
   // base: '/', // 格式：'/<仓库名>/'，必需项
@@ -22,8 +27,8 @@ module.exports = {
       repo: 'vuepress-theme-deeply',
       clientId: '612c892c769627382be8', //GitHub page
       clientSecret: '61e933f88b95f35e462f27243ea3dce33b6bfac9',
-//       clientId: '4185e1a31e80d0b76772', //本地
-//       clientSecret: '95eed2195d27127a5e5401dd91d20b6de26fdf39',
+      //       clientId: '4185e1a31e80d0b76772', //本地
+      //       clientSecret: '95eed2195d27127a5e5401dd91d20b6de26fdf39',
       locale: 'zh'
     },
   },

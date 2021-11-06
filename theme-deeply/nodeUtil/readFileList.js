f4ed31028fec0d56038e68bd5d6b66fbd331c8d6
halodong <1661089448@qq.com>
2021-11-06 16:33:23 +0800

    fix: url error

--- theme-deeply/nodeUtil/readFileList.js
+++ theme-deeply/nodeUtil/readFileList.js
@@ -14,32 +14,34 @@ function readFileList(dir, filesList = []) {
     if (stat.isDirectory() && item !== ".vuepress" && item !== "@pages") {
       readFileList(path.join(dir, item), filesList); //递归读取文件
     } else {
-      const fileNameArr = path.basename(filePath).split(".");
-      let name = null,
-        type = null;
-      if (fileNameArr.length === 2) {
-        // 没有序号的文件
-        name = fileNameArr[0];
-        type = fileNameArr[1];
-      } else if (fileNameArr.length === 3) {
-        // 有序号的文件
-        name = fileNameArr[1];
-        type = fileNameArr[2];
-      } else {
-        // 超过两个‘.’的
-        log(
-          chalk.yellow(
-            `warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`
-          )
-        );
-        return;
-      }
-      if (type === "md") {
-        // 过滤非md文件
-        filesList.push({
-          name,
-          filePath,
-        });
+      if (path.basename(dir) !== "docs") {
+        const fileNameArr = path.basename(filePath).split(".");
+        let name = null,
+          type = null;
+        if (fileNameArr.length === 2) {
+          // 没有序号的文件
+          name = fileNameArr[0];
+          type = fileNameArr[1];
+        } else if (fileNameArr.length === 3) {
+          // 有序号的文件
+          name = fileNameArr[1];
+          type = fileNameArr[2];
+        } else {
+          // 超过两个‘.’的
+          log(
+            chalk.yellow(
+              `warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`
+            )
+          );
+          return;
+        }
+        if (type === "md") {
+          // 过滤非md文件
+          filesList.push({
+            name,
+            filePath,
+          });
+        }
       }
     }
   });

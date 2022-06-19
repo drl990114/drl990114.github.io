0f8adea5f96a9e479c45f858e360f5ab4e96ff42
halodong <drl990114@gmail.com>
2022-06-19 22:48:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:halodong/halodong.github.io.git gh-pages
+git push -f git@github.com:halodong/halodong.github.io.git master:gh-pages
 
 cd -

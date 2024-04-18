8685b84de9e376937b9c30261c1dfc65c62f6da5
drl990114 <drl990114@users.noreply.github.com>
2024-04-18 15:07:17 +0000

    deploy: e106f11245985b6ec2866d226b142c1406a56960

---
+++ assets/posts_React_实现react组件的递归调用.md.dbdfb4b3.js
@@ -0,0 +1,93 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.461276d4.js";const m=JSON.parse('{"title":"实现react组件的递归调用","description":"","frontmatter":{"title":"实现react组件的递归调用","date":"2021-05-14T21:20:34.000Z","permalink":"/read/2021051401","author":"RuiLin Dong","categories":["React"],"tags":["React"]},"headers":[],"relativePath":"posts/React/实现react组件的递归调用.md","filePath":"posts/React/实现react组件的递归调用.md"}'),p={name:"posts/React/实现react组件的递归调用.md"},o=l(`<p>使用React来简单实现下组件的递归。在React可以通过props和children来向子组件传递信息，不过在这里大致的思路都是一样的。我们先定义一下要渲染的json结构：</p><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">list </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> [</span></span>
+<span class="line"><span style="color:#E1E4E8;">    {</span></span>
+<span class="line"><span style="color:#E1E4E8;">        name: </span><span style="color:#9ECBFF;">&#39;1&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        children: [</span></span>
+<span class="line"><span style="color:#E1E4E8;">        {</span></span>
+<span class="line"><span style="color:#E1E4E8;">            name: </span><span style="color:#9ECBFF;">&#39;1-1&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">        },</span></span>
+<span class="line"><span style="color:#E1E4E8;">        {</span></span>
+<span class="line"><span style="color:#E1E4E8;">            name: </span><span style="color:#9ECBFF;">&#39;1-2&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">            children: [</span></span>
+<span class="line"><span style="color:#E1E4E8;">            {</span></span>
+<span class="line"><span style="color:#E1E4E8;">                name: </span><span style="color:#9ECBFF;">&#39;1-2-1&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">            }</span></span>
+<span class="line"><span style="color:#E1E4E8;">            ]</span></span>
+<span class="line"><span style="color:#E1E4E8;">        },</span></span>
+<span class="line"><span style="color:#E1E4E8;">        {</span></span>
+<span class="line"><span style="color:#E1E4E8;">            name: </span><span style="color:#9ECBFF;">&#39;1-3&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">        }</span></span>
+<span class="line"><span style="color:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="color:#E1E4E8;">    },</span></span>
+<span class="line"><span style="color:#E1E4E8;">    {</span></span>
+<span class="line"><span style="color:#E1E4E8;">        name: </span><span style="color:#9ECBFF;">&#39;2&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        children: [</span></span>
+<span class="line"><span style="color:#E1E4E8;">        {</span></span>
+<span class="line"><span style="color:#E1E4E8;">            name: </span><span style="color:#9ECBFF;">&#39;2-1&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">        }</span></span>
+<span class="line"><span style="color:#E1E4E8;">        ]</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">]</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">list </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> [</span></span>
+<span class="line"><span style="color:#24292E;">    {</span></span>
+<span class="line"><span style="color:#24292E;">        name: </span><span style="color:#032F62;">&#39;1&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">        children: [</span></span>
+<span class="line"><span style="color:#24292E;">        {</span></span>
+<span class="line"><span style="color:#24292E;">            name: </span><span style="color:#032F62;">&#39;1-1&#39;</span></span>
+<span class="line"><span style="color:#24292E;">        },</span></span>
+<span class="line"><span style="color:#24292E;">        {</span></span>
+<span class="line"><span style="color:#24292E;">            name: </span><span style="color:#032F62;">&#39;1-2&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">            children: [</span></span>
+<span class="line"><span style="color:#24292E;">            {</span></span>
+<span class="line"><span style="color:#24292E;">                name: </span><span style="color:#032F62;">&#39;1-2-1&#39;</span></span>
+<span class="line"><span style="color:#24292E;">            }</span></span>
+<span class="line"><span style="color:#24292E;">            ]</span></span>
+<span class="line"><span style="color:#24292E;">        },</span></span>
+<span class="line"><span style="color:#24292E;">        {</span></span>
+<span class="line"><span style="color:#24292E;">            name: </span><span style="color:#032F62;">&#39;1-3&#39;</span></span>
+<span class="line"><span style="color:#24292E;">        }</span></span>
+<span class="line"><span style="color:#24292E;">        ]</span></span>
+<span class="line"><span style="color:#24292E;">    },</span></span>
+<span class="line"><span style="color:#24292E;">    {</span></span>
+<span class="line"><span style="color:#24292E;">        name: </span><span style="color:#032F62;">&#39;2&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">        children: [</span></span>
+<span class="line"><span style="color:#24292E;">        {</span></span>
+<span class="line"><span style="color:#24292E;">            name: </span><span style="color:#032F62;">&#39;2-1&#39;</span></span>
+<span class="line"><span style="color:#24292E;">        }</span></span>
+<span class="line"><span style="color:#24292E;">        ]</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"><span style="color:#24292E;">]</span></span></code></pre></div><p>我们的Item组件可以这样实现：</p><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#B392F0;">render</span><span style="color:#E1E4E8;">() {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">const</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">list</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">any</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">this</span><span style="color:#E1E4E8;">.props.children </span><span style="color:#F97583;">||</span><span style="color:#E1E4E8;"> [];</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> (</span></span>
+<span class="line"><span style="color:#E1E4E8;">        &lt;</span><span style="color:#85E89D;">div</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">className</span><span style="color:#F97583;">=</span><span style="color:#9ECBFF;">&quot;item&quot;</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">            {</span></span>
+<span class="line"><span style="color:#E1E4E8;">                list.</span><span style="color:#B392F0;">map</span><span style="color:#E1E4E8;">((</span><span style="color:#FFAB70;">item</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">ItemData</span><span style="color:#E1E4E8;">, </span><span style="color:#FFAB70;">index</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">number</span><span style="color:#E1E4E8;">) </span><span style="color:#F97583;">=&gt;</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">                    </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> &lt;</span><span style="color:#79B8FF;">React.Fragment</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">key</span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;">{index}&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">                        &lt;</span><span style="color:#85E89D;">h3</span><span style="color:#E1E4E8;">&gt;{item.name}&lt;/</span><span style="color:#85E89D;">h3</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">                        {</span></span>
+<span class="line"><span style="color:#E1E4E8;">                            </span><span style="color:#6A737D;">// 当该节点还有children时，则递归调用本身</span></span>
+<span class="line"><span style="color:#E1E4E8;">                            item.children </span><span style="color:#F97583;">&amp;&amp;</span><span style="color:#E1E4E8;"> item.children.</span><span style="color:#79B8FF;">length</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">?</span></span>
+<span class="line"><span style="color:#E1E4E8;">                            &lt;</span><span style="color:#79B8FF;">Item</span><span style="color:#E1E4E8;">&gt;{item.children}&lt;/</span><span style="color:#79B8FF;">Item</span><span style="color:#E1E4E8;">&gt; </span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">null</span></span>
+<span class="line"><span style="color:#E1E4E8;">                        }</span></span>
+<span class="line"><span style="color:#E1E4E8;">                    &lt;/</span><span style="color:#79B8FF;">React.Fragment</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">                })</span></span>
+<span class="line"><span style="color:#E1E4E8;">            }</span></span>
+<span class="line"><span style="color:#E1E4E8;">        &lt;/</span><span style="color:#85E89D;">div</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    )</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6F42C1;">render</span><span style="color:#24292E;">() {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">const</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">list</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">any</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">this</span><span style="color:#24292E;">.props.children </span><span style="color:#D73A49;">||</span><span style="color:#24292E;"> [];</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> (</span></span>
+<span class="line"><span style="color:#24292E;">        &lt;</span><span style="color:#22863A;">div</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">className</span><span style="color:#D73A49;">=</span><span style="color:#032F62;">&quot;item&quot;</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">            {</span></span>
+<span class="line"><span style="color:#24292E;">                list.</span><span style="color:#6F42C1;">map</span><span style="color:#24292E;">((</span><span style="color:#E36209;">item</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">ItemData</span><span style="color:#24292E;">, </span><span style="color:#E36209;">index</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">number</span><span style="color:#24292E;">) </span><span style="color:#D73A49;">=&gt;</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">                    </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> &lt;</span><span style="color:#005CC5;">React.Fragment</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">key</span><span style="color:#D73A49;">=</span><span style="color:#24292E;">{index}&gt;</span></span>
+<span class="line"><span style="color:#24292E;">                        &lt;</span><span style="color:#22863A;">h3</span><span style="color:#24292E;">&gt;{item.name}&lt;/</span><span style="color:#22863A;">h3</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">                        {</span></span>
+<span class="line"><span style="color:#24292E;">                            </span><span style="color:#6A737D;">// 当该节点还有children时，则递归调用本身</span></span>
+<span class="line"><span style="color:#24292E;">                            item.children </span><span style="color:#D73A49;">&amp;&amp;</span><span style="color:#24292E;"> item.children.</span><span style="color:#005CC5;">length</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">?</span></span>
+<span class="line"><span style="color:#24292E;">                            &lt;</span><span style="color:#005CC5;">Item</span><span style="color:#24292E;">&gt;{item.children}&lt;/</span><span style="color:#005CC5;">Item</span><span style="color:#24292E;">&gt; </span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">null</span></span>
+<span class="line"><span style="color:#24292E;">                        }</span></span>
+<span class="line"><span style="color:#24292E;">                    &lt;/</span><span style="color:#005CC5;">React.Fragment</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">                })</span></span>
+<span class="line"><span style="color:#24292E;">            }</span></span>
+<span class="line"><span style="color:#24292E;">        &lt;/</span><span style="color:#22863A;">div</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">    )</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div><p>最后调用这个组件：</p><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">&lt;</span><span style="color:#79B8FF;">Item</span><span style="color:#E1E4E8;">&gt;{</span><span style="color:#79B8FF;">this</span><span style="color:#E1E4E8;">.list}&lt;/</span><span style="color:#79B8FF;">Item</span><span style="color:#E1E4E8;">&gt;</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">&lt;</span><span style="color:#005CC5;">Item</span><span style="color:#24292E;">&gt;{</span><span style="color:#005CC5;">this</span><span style="color:#24292E;">.list}&lt;/</span><span style="color:#005CC5;">Item</span><span style="color:#24292E;">&gt;</span></span></code></pre></div>`,6),e=[o];function c(t,E,r,y,i,F){return n(),a("div",null,e)}const h=s(p,[["render",c]]);export{m as __pageData,h as default};
